d654250357c05283b637409f86a50267f4ed94f9
VIVEK GUPTA <vivek.jmdsmvdu@gmail.com>
2021-05-26 21:53:51 +0530

    done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,42 @@ import { DataBindingComponent } from './data-binding/data-binding.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http';
 import { MeanComponent } from './mean/mean.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { ProductComponent } from './product/product.component';
+import { ContactComponent } from './contact/contact.component';
+import { RouterModule, Routes } from '@angular/router';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+
+const routes:Routes = [
+  {path:'', component: HomeComponent},
+  {path:'about', component: AboutComponent},
+  {path:'product', component: ProductComponent},
+  {path:'contact', component: ContactComponent},
+  {path: '**', component:PageNotFoundComponent}
+]
+
+
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     DataBindingComponent,
-    MeanComponent
+    MeanComponent,
+    HomeComponent,
+    AboutComponent,
+    ProductComponent,
+    ContactComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
     BrowserAnimationsModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(routes)
   ],
   providers: [],
   bootstrap: [AppComponent],
